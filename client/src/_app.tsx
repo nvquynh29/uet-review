88eb5885ce78a636c9118a114912719173c368a8
nvquynh29 <nvquynh29@gmail.com>
2022-10-23 22:18:11 +0700

    init project

---
+++ client/src/_app.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+interface IAppProps {
+}
+
+const App: React.FunctionComponent<IAppProps> = (props) => {
+  return <div>Welcome to UET-Review</div>;
+};
+
+export default App;
---
+++ server/src/index.ts
@@ -0,0 +1,36 @@
+import express, { Application, Request, Response, NextFunction } from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+dotenv.config()
+
+const PORT = process.env.PORT
+const DATABASE_URI = process.env.DATABASE_URI
+
+const app: Application = express()
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('Welcome to UET-Review')
+})
+
+// middleware
+app.use(bodyParser.json({ limit: '30mb' }))
+app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }))
+app.use(cors())
+
+mongoose
+  .connect(<string>DATABASE_URI, <mongoose.ConnectOptions>{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to DB')
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.error(err)
+  })
