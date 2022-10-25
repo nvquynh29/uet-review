453d45feccbd4d62faabd36efebab5e0b92d47f5
nvquynh29 <nvquynh29@gmail.com>
2022-10-25 17:39:06 +0700

    feat: dockerize app

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,17 +20,21 @@ app.use(bodyParser.json({ limit: '30mb' }))
 app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }))
 app.use(cors())
 
-mongoose
-  .connect(<string>DATABASE_URI, <mongoose.ConnectOptions>{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Connected to DB')
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`)
-    })
-  })
-  .catch((err) => {
-    console.error(err)
-  })
+// mongoose
+//   .connect(<string>DATABASE_URI, <mongoose.ConnectOptions>{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log('Connected to DB')
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT}`)
+//     })
+//   })
+//   .catch((err) => {
+//     console.error(err)
+//   })
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
