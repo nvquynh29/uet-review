b11836c517bf2265a26bce5773d0cb6bf51a52ae
nvquynh29 <nvquynh29@gmail.com>
2022-10-26 23:35:08 +0700

    chore: update ui homepage

--- client/src/_app.tsx
+++ client/src/_app.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
+import "./styles.css";
 
 interface IAppProps {
 }
 
 const App: React.FunctionComponent<IAppProps> = (props) => {
-  return <div>Welcome to UET-Review</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="title">Welcome to UET Review</div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
