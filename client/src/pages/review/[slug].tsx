16932f0160a7b5b8ff257c00b1d21e21776ad016
Devid2207 <devid220701@gmail.com>
2022-10-30 14:02:22 +0700

    set up route - login form - register form - header - create review form

---
+++ client/src/pages/review/[slug].tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import { useParams } from 'react-router-dom'
+
+// import { IParams } from '../../utils/TypeScript'
+
+const DetailReview = () => {
+  // const id = useParams<IParams>().slug
+
+  return (
+    <div className="my-4">
+      <h2>Detail Review</h2>
+    </div>
+  )
+}
+
+export default DetailReview
