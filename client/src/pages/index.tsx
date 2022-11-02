a5d1135a315d37a4967c86d0a6aaf61256e71b34
nvquynh29 <nvquynh29@gmail.com>
2022-11-02 23:57:49 +0700

    feat: view list post

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
+import { getListPost } from '../api/post'
+import { Post, Author } from './review/[slug]'
 
 const Home = () => {
+  const [posts, setPosts] = useState<[{post: Post, author: Author}]>()
+
+  useEffect(() => {
+    fetchListPost()
+  }, [])
+  
+  const fetchListPost = useCallback(
+    async () => {
+      try {
+        const { data } = await getListPost()
+        setPosts(data)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    [],
+  )
+  
   return (
     <div>
-      <h2>Home Page</h2>
+      <h2 className="text-center">Home Page</h2>
+      <div className="list-post mt-4">
+        {posts?.map(item => (
+        <div key={item.post._id} className="card">
+          <div className="card-body">
+            <a href={`/review/${item.post.slug}`}>{item.post.title}</a>
+          </div>
+        </div>
+      ))}
+      </div>
     </div>
   )
 }
