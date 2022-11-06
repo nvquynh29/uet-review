258f894484c91fdcabf5427fa53672b31d6ba5a7
nvquynh29 <nvquynh29@gmail.com>
2022-11-07 00:01:32 +0700

    feat: api comment post, get post with comments

--- server/src/api/v1/controllers/post.controller.ts
+++ server/src/api/v1/controllers/post.controller.ts
@@ -3,6 +3,7 @@ import { Types } from 'mongoose'
 import Post, { IPost, IReview } from '../models/post.model'
 import { generateSlug } from '../../../pkg/slugify'
 import { IPagination } from '../types'
+import Comment, { IComment } from '../models/comment.model'
 
 const createPost = async (req: Request, res: Response) => {
   const body = req.body
@@ -43,11 +44,13 @@ const getPost = async (req: Request, res: Response) => {
     return res.sendStatus(404)
   }
 
+  const comments = await Comment.find({ post_id: post._id })
+
   const author = {
     nickname: 'Trịnh Mai Huy',
     email: 'trinh.mai.huy@gmail.com',
   }
-  return res.json({ data: { post, author } })
+  return res.json({ data: { post, author, comments } })
 }
 
 const getListPost = async (req: Request, res: Response, next: NextFunction) => {
@@ -66,6 +69,7 @@ const getListPost = async (req: Request, res: Response, next: NextFunction) => {
     pagination.total_page = totalPage
 
     const data: any = []
+    // TODO: Use real data
     const author = {
       nickname: 'Trịnh Mai Huy',
       email: 'trinh.mai.huy@gmail.com',
@@ -83,4 +87,32 @@ const getListPost = async (req: Request, res: Response, next: NextFunction) => {
   }
 }
 
-export { createPost, getPost, getListPost }
+const commentPost = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const post = await Post.find({ _id: id })
+    if (post) {
+      const { content } = req.body
+      // TODO: using validation instead
+      if (!content) {
+        return res.sendStatus(400)
+      }
+      const comment: IComment = {
+        author_id: new Types.ObjectId('636125870f506d29b2baf95b'),
+        post_id: new Types.ObjectId(id),
+        content,
+      }
+      const newComment = await Comment.create(comment)
+      const data = {
+        ...newComment.toJSON(),
+        author: 'Đỗ Tiến Đạt',
+      }
+      return res.json({ data })
+    }
+    return res.sendStatus(404)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+export { createPost, getPost, getListPost, commentPost }
--- server/src/api/v1/routes/v1/post.route.ts
+++ server/src/api/v1/routes/v1/post.route.ts
@@ -6,5 +6,6 @@ const postRouter: Router = express.Router()
 postRouter.post('/', PostController.createPost)
 postRouter.get('/', PostController.getListPost)
 postRouter.get('/:slug', PostController.getPost)
+postRouter.post('/:id/comment', PostController.commentPost)
 
 export default postRouter
