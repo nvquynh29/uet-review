de1fb70ccaeada5ce34d46f37336b686bd91ec58
nvquynh29 <nvquynh29@gmail.com>
2022-11-07 20:45:20 +0700

    chore: update api config for FE

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios'
 
 const instance: AxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_V1_URL || 'http://localhost:5500/v1',
+  baseURL: process.env.REACT_APP_API_URL,
 })
 
 export default instance
